aeac2027a0d12cb7d62fefed39584c157d0481df
HaiDuong2035 <dfg.hd.2035@gmail.com>
2025-03-30 10:13:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title('Tạo tài khoản')
+ten=st.text_input('Tên đăng nhập')
+mk=st.text_input('Mật khẩu')
+bar=st.progress(0)
+if ten=='' and mk=='' :
+  bar.progress(0)
+elif ten=='' or mk=='':
+  bar.progress(50)
+else:
+  bar.progress(100)
