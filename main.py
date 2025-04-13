b21416b0c8d16523d19acc03cbfe0e39b003229f
HaiDuong2035 <dfg.hd.2035@gmail.com>
2025-04-13 11:10:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import streamlit as st
-st.title('Tạo tài khoản')
-x=0
-a=st.text_input('Tài khoản')
-if a!='':
-  x+=20
-b=st.text_input('Mật khẩu')
-if b!='':
-  x+=20
-c=st.text_input('Nhập lại mật khẩu')
-if c!='':
-  x+=20
-d=st.text_input('Tên người dùng')
-if d!='':
-  x+=20
-e=st.text_input('Email')
-if e!='':
-  x+=20
-st.progress(x)
+st.set_page_config(page_title='Trac nghiem kiem tra tinh cach',page_icon='',layout='wide')
+st.title('Trac nghiem kiem tra tinh cach')
+dict={
+'':'ban chua chon con gi',
+'meo':'lua chon nay cho thay ban chua san sang de bat dau cong viec, khao khat duoc di nghi',
+'cho':'ban cam nhan duoc su ho tro nhiet tinh cua ban be va vi the nen san sang giai quyet moi van de xay ra',
+'sutu':'Co the thay ban la nguoi co ve ngoai noi bat, thu hut mn bang ve hao nhoang',
+'ngua':'Co dieu gi do dang han che su tu do cua ban',
+'thiennga':'hien tai ban co khoang thoi gian ngot ngao, co gang tan huong va keo dai no.'}
+choice=''
+c1,c2,c3,c4,c5=st.columns(5)
+with c1:
+  if st.button('Meo'):
+    choice='meo'
+with c2:
+  if st.button('Cho'):
+    choice='cho'
+with c3:
+  if st.button('Su tu'):
+    choice='sutu'
+with c4:
+  if st.button('Ngua'):
+    choice='ngua'
+with c5:
+  if st.button('Thien nga'):
+    choice='thiennga'
+st.write(dict[choice])
+with st.sidebar:
+  st.write('Trac nghiem kiem tra tinh cach')
+  if choice=='':
+    st.write('ban chua chon con gi')
+  else:
+    st.write('Ban da chon con',choice)
